5c53553bf9218c66bdf4d573cd69a78ec6366182
Treagan Birbal <treaganbirbal@pursuit.org>
2019-12-03 18:31:28 -0500

    hands transition

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
+    let secHand = document.querySelector('.secHand')
+    let minHand = document.querySelector('.minHand')
+    let hrHand = document.querySelector('.hrHand')
+    let hands = document.getElementsByClassName('.hand')
+
+    let time = new Date()
+
+    // window.setInterval(() => {
+    // console.log(time), 5000})
     
+    document.querySelectorAll('.hand')[2].addEventListener('click', (event) => {
+       console.log(event.target)
+    })
 })
