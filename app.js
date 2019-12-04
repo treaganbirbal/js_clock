418085534335e3c64792e7feecce4ee40c52550f
Treagan Birbal <treaganbirbal@pursuit.org>
2019-12-03 19:04:06 -0500

    hrs

--- app.js
+++ app.js
@@ -14,10 +14,15 @@ document.addEventListener("DOMContentLoaded", () => {
         const secondsDegrees = ((seconds/60) * 360) + 90;
         secHand.style.transform = `rotate(${secondsDegrees}deg)`
 
-
         const minutes = now.getMinutes()
         const minsDegrees = ((minutes/60) * 360) + 90;
         minHand.style.transform = `rotate(${minsDegrees}deg)`
+
+        const hours = now.getHours() - 12
+        const hrDegrees = ((hours / 12) * 360) + 90;
+        hrHand.style.transform = `rotate(${hrDegrees}deg)`
+
+        console.log(hours)
     }
 
   setInterval(setDate, 1000)
